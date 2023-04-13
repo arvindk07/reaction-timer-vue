fc583ed6a2d9fe92493ce9509e746afdaa268053
arvindk07 <info.arvind07@gmail.com>
2023-04-13 14:11:43 +0530

    starting a new game

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>The Engineer Babu Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Block from "./components/Block.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Block,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 50000;
+      this.isPlaying = true;
+    },
+  },
+};
 </script>
 
 <style>
